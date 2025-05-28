f8c1c1ccd98e005469e0d74b97da7a8f42da3a20
spacegt <541980655@qq.com>
2025-05-28 20:35:18 +0800

    增加 缩放按钮

--- src/components/editor/TimelineTool.vue
+++ src/components/editor/TimelineTool.vue
@@ -3,8 +3,9 @@
         <div>
             {{ timelineScaleStore.zoom }}
         </div>
-        <v-slider v-model="timelineScaleStore.zoom" thumb-label :max="100" :min="1" :step="1" max-width="1000px"
-            density="compact" size="small" hide-details></v-slider>
+        <v-slider v-model="timelineScaleStore.zoom" thumb-label :max="100" :min="1" :step="1" max-width="200px"
+            density="compact" size="small" hide-details reverse append-icon="mdi-magnify-plus-outline"
+            prepend-icon="mdi-magnify-minus-outline" @click:append="zoomIn" @click:prepend="zoomOut"></v-slider>
     </div>
 </template>
 
@@ -12,6 +13,16 @@
 import { useTimelineScaleStore } from '@/stores/editor/timeline-scale';
 
 const timelineScaleStore = useTimelineScaleStore()
+
+const zoomOut = () => {
+    timelineScaleStore.zoom += 10
+    if (timelineScaleStore.zoom > 100) timelineScaleStore.zoom = 100
+
+}
+const zoomIn = () => {
+    timelineScaleStore.zoom -= 10
+    if (timelineScaleStore.zoom < 0) timelineScaleStore.zoom = 0
+}
 
 </script>
 
